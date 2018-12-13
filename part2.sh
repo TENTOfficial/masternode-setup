055f80c39f8c1c9642f40e621259bf555345dd0a
SnowGem <ceo@snowgem.org>
2018-12-13 14:43:52 +0700

    update script

--- part2.sh
+++ part2.sh
@@ -63,13 +63,27 @@ chmod +x ~/snowgemd ~/snowgem-cli
 #start
 systemctl enable --now snowgem.service
 
-echo "wait for 200 seconds"
 x=1
-while [ $x -le 200 ]
-do
-  echo "$x"
-  sleep 1
-  x=$(( $x + 1 ))
+echo "wait for starting"
+while true ; do
+    echo "Please wait ($x)"
+    sleep 1
+    x=$(( $x + 1 ))
+    if ./snowgem-cli getinfo | grep '"difficulty"' ; then
+        ./snowgem-cli getinfo
+        echo "checking masternode status"
+        while true ; do
+            echo "Please wait ($x)"
+            sleep 1
+            x=$(( $x + 1 ))
+            if ! ./snowgem-cli masternodedebug | grep -q 'not yet activated'; then
+                ./snowgem-cli masternodedebug
+                break
+            fi
+        done
+        break
+    fi
 done
 
 ./snowgem-cli getinfo
+./snowgem-cli masternodedebug

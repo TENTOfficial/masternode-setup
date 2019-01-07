216250d10c840f3169bd9f0060aa6e1c28fb8943
SnowGem <ceo@snowgem.org>
2019-01-08 01:46:07 +0700

    update part2 script

--- part2.sh
+++ part2.sh
@@ -63,8 +63,11 @@ if [ ! -d ~/.snowgem/blocks ]; then
   wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part2 -O ~/bc.sf-part2
   wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part3 -O ~/bc.sf-part3
   wget -N https://github.com/Snowgem/Data/releases/download/0.0.1/blockchain_snowgem_index.zip.sf-part4 -O ~/bc.sf-part4
-  rm ~/data -r
+  sudo rm ~/data -r
   git clone https://github.com/Snowgem/Data ~/data
+  cd ~/data
+  npm install
+  cd ~
   node ~/data/joinfile.js
   rm ~/bc.sf-part1
   rm ~/bc.sf-part2

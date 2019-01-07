b7660d245943f305d668d211de149483787a8923
SnowGem <ceo@snowgem.org>
2019-01-08 03:31:19 +0700

    update script

--- part2.sh
+++ part2.sh
@@ -91,18 +91,22 @@ while true ; do
     sleep 1
     x=$(( $x + 1 ))
     ./snowgem-cli getinfo &> text.txt
-    line=$(head -n 1 text.txt)
-    if [[ $line == *"connect to server: unknown"* ]]; then
+    line=$(tail -n 1 text.txt)
+    if [[ $line == *"..."* ]]; then
+        echo $line
+    fi
+    if [[ $(tail -n 1 text.txt) == *"sure server is running"* ]]; then
         echo "Cannot start wallet, please contact us on Discord(https://discord.gg/7a7XRZr) for help"
         break
-    elif [[ $line != *"error code"*  ]] && [[ $line == *"{"*  ]]; then
-        ./snowgem-cli getinfo
+    elif [[ $(head -n 20 text.txt) == *"version"*  ]]; then
         echo "Checking masternode status"
         while true ; do
             echo "Please wait ($x)"
             sleep 1
             x=$(( $x + 1 ))
-            if ! ./snowgem-cli masternodedebug | grep -q 'not yet activated'; then
+            ./snowgem-cli masternodedebug &> text.txt
+            line=$(head -n 1 text.txt)
+            if [[ $line == *"not yet activated"* ]]; then
                 ./snowgem-cli masternodedebug
                 break
             fi
